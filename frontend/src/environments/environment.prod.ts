ed80aeece4cc98c0eea8ce71704b76252c23fe70
joshpg190498 <joseperez1910498@gmail.com>
2024-11-01 17:48:46 -0500

    docker nginx

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   api: {
-    uri: 'http://192.168.1.210:3000/graphql'
+    uri: 'http://192.168.1.210/api/graphql',
+    wsUri: 'wss://192.168.1.210/api/subscriptions'
   }
 };
