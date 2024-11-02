1a5b2ae8558a39bd81c2f74aa5eddacbf5ed5526
joshpg190498 <joseperez1910498@gmail.com>
2024-11-02 02:32:06 -0500

    changes on docker and environment frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   api: {
-    uri: 'http://192.168.1.210/api/graphql',
+    uri: 'https://192.168.1.210/api/graphql',
     wsUri: 'wss://192.168.1.210/api/subscriptions'
   }
 };
